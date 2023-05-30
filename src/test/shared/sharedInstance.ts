731c11638ddcaa22a4a9de34fb0895ed68409eb3
Quan Nguyen <quan.nguyen.suomea@gmail.com>
2023-05-30 14:44:14 +0300

    update link

--- src/test/shared/sharedInstance.ts
+++ src/test/shared/sharedInstance.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'https://api.escuelajs.co/api/v1/',
-  baseURL: 'http://localhost:5002/'
+  baseURL: 'https://acacia-backend.azurewebsites.net/'
 });
 
 export const backupAxiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://localhost:5002',
 });
 
 export default axiosInstance;
